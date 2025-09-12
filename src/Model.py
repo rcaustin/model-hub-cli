1de30ae2290f206a460b6cd7a95954d9550b584e
Chatfie <codyhatfield70@gmail.com>
2025-09-12 11:39:46 -0400

    ModelCategory can now calculate a Model's ndjson. generateReports() will print all models ndjsons

--- src/Model.py
+++ src/Model.py
@@ -1,30 +1,56 @@
-from typing import Optional, Union
+from typing import Union
 from Metric import Metric
+import time
 
 
 class Model:
 
-    """
-    evaluations maps metric names to their scores.
-    Scores can be a float or a dictionary of floats for complex metrics.
-    """
     def __init__(
         self,
         name: str,
         modelLink: str,
         codeLink: str,
-        datasetLink: str,
-        evaluations: Optional[dict[str, Union[float, dict[str, float]]]]
+        datasetLink: str
     ):
         self.name = name
         self.modelLink = modelLink
         self.codeLink = codeLink
         self.datasetLink = datasetLink
-        self.evaluations = evaluations if evaluations is not None else {}
 
-    def evaluate(self, metric: Metric):
+        """
+        evaluations maps metric names to their scores.
+        Scores can be a float or a dictionary of floats for complex metrics.
+        evaluationsLatency maps metric names to the time taken to compute them.
+        """
+        self.evaluations: dict[str, Union[float, dict[str, float]]] = {}
+        self.evaluationsLatency: dict[str, float] = {}
+
+    def evaluate(self, metric: Metric) -> None:
+
+        # Evaluate the given metric and record its score and evaluation time.
+        start: float = time.time()
         score: Union[float, dict[str, float]] = metric.evaluate()
-        self.evaluations[type(metric).__name__] = score
+        end: float = time.time()
+        elapsed: float = end - start
+
+        # Record the evaluation results.
+        metric_name: str = type(metric).__name__
+        self.evaluations[metric_name] = score
+        self.evaluationsLatency[metric_name] = elapsed
 
     def getEvals(self) -> dict[str, Union[float, dict[str, float]]]:
         return self.evaluations
+
+    def getEvalsLatency(self) -> dict[str, float]:
+        return self.evaluationsLatency
+    
+    def getCategory(self) -> str:
+        categories = []
+        if self.modelLink:
+            categories.append("MODEL")
+        if self.datasetLink:
+            categories.append("DATASET")
+        if self.codeLink:
+            categories.append("CODE")
+        return f"[{', '.join(categories)}]"
+
