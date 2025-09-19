244d8430ff599aca672622aff3dedb46a9766abc
Ryan Austin <ryan.rcaustin@gmail.com>
2025-09-19 11:33:52 -0400

    Added cached API vars to ModelData

--- src/Interfaces.py
+++ src/Interfaces.py
@@ -1,7 +1,21 @@
-from typing import Protocol, Optional
+from abc import abstractmethod
+from typing import Any, Dict, Optional, Protocol
 
 
 class ModelData(Protocol):
+
     modelLink: str
     codeLink: Optional[str]
     datasetLink: Optional[str]
+
+    @property
+    @abstractmethod
+    def hf_metadata(self) -> Optional[Dict[str, Any]]:
+        """Cached HuggingFace metadata"""
+        ...
+
+    @property
+    @abstractmethod
+    def github_metadata(self) -> Optional[Dict[str, Any]]:
+        """Cached GitHub metadata"""
+        ...
